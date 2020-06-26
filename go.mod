ae6a2e59d8007e120d55b8e197918c9fad11026a
Lucas França <lucasfranca.dev@gmail.com>
2020-06-26 07:59:29 -0300

    test pagseguro interface

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/upowl/pagseguro
+
+go 1.14
+
+require github.com/stretchr/testify v1.6.1
---
+++ pagseguro_test.go
@@ -0,0 +1,35 @@
+package pagseguro
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPagseguroProduction(t *testing.T) {
+	input := PagseguroInput{
+		Env:   0,
+		Email: "email@email.com.br",
+		Token: "98ouyhbouihb07ho8yb879b",
+	}
+
+	pag, errPag := New(&input)
+	assert.Nil(t, errPag, "this return err is nil")
+	assert.Equal(t, "email@email.com.br", pag.GetEmail(), "this return is email")
+	assert.Equal(t, "98ouyhbouihb07ho8yb879b", pag.GetToken(), "this return is token")
+	assert.Equal(t, productionURL, pag.GetURL(), "this return is url production")
+}
+
+func TestPagseguroSandbox(t *testing.T) {
+	input := PagseguroInput{
+		Env:   1,
+		Email: "email@email.com.br",
+		Token: "98ouyhbouihb07ho8yb879b",
+	}
+
+	pag, errPag := New(&input)
+	assert.Nil(t, errPag, "this return err is nil")
+	assert.Equal(t, "email@email.com.br", pag.GetEmail(), "this return is email")
+	assert.Equal(t, "98ouyhbouihb07ho8yb879b", pag.GetToken(), "this return is token")
+	assert.Equal(t, sandboxURL, pag.GetURL(), "this return is url sandbox")
+}
